ff73767ca4eff2f93a2a4f6deabaf2dacd2572a9
Roy <roy0983617927@icloud.com>
2026-05-20 12:57:15 +0800

    HitLines red

--- ReSlotGameTests/ReSlotGameNormalTests.cs
+++ ReSlotGameTests/ReSlotGameNormalTests.cs
@@ -18,4 +18,46 @@ public class ReSlotGameNormalTests
         var win = slot.Calculate(10);
         Assert.Equal(0, win);
     }
+    [Fact]
+    public void HitOneLine()
+    {
+        var slot = new SlotMachine(new List<List<string>>()
+        {
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "9", "Q", "10", "A","K"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+        });
+        var win = slot.Calculate(10);
+        Assert.Equal(100, win);
+    }
+    [Fact]
+    public void HitTwoLines()
+    {
+        var slot = new SlotMachine(new List<List<string>>()
+        {
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "Q", "10", "K", "A","9"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+        });
+        var win = slot.Calculate(10);
+        Assert.Equal(400, win);
+    } 
+    [Fact]
+    public void HitThreeLines()
+    {
+        var slot = new SlotMachine(new List<List<string>>()
+        {
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "Q", "K", "10", "A","9"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+        });
+        var win = slot.Calculate(10);
+        Assert.Equal(1000, win);
+    }
 }
