a77e8745b9b6eaad70e65df42a254e2036027eb2
Roy <roy0983617927@icloud.com>
2026-05-20 15:40:07 +0800

    screen red

--- Domain/Screen.cs
+++ Domain/Screen.cs
@@ -2,5 +2,28 @@ namespace Domain;
 
 public class Screen
 {
-    
+    private  List<List<string>> _wheels { get; private set; }
+    private  readonly INumberGenerator _numberGenerator;
+
+    public Screen(List<List<string>> wheels, INumberGenerator numberGenerator)
+    {
+        _numberGenerator = numberGenerator;
+        _wheels = wheels;
+    }
+
+    private List<List<string>> GetScreen(List<List<string>> wheels)
+    {
+        var screen =  new List<List<string>>();
+        for (int i = 0; i < wheels.Count; i++)
+        {
+            int nextPosition = _numberGenerator.Gernerate();
+
+            var column = wheels[i].Concat(wheels[i])
+                .ToList()
+                .GetRange(nextPosition, 3);
+            screen.Add(column);
+        }
+
+        return screen;
+    }
 }
