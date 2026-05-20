d0383591ca0329e38db8f273268819426875ec40
Roy <roy0983617927@icloud.com>
2026-05-20 12:22:33 +0800

    Loss red

---
+++ ReSlotGameTests/ReSlotGameNormalTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ReSlotGameTests;
+
+using Domain;
+
+public class ReSlotGameNormalTests
+{
+    [Fact]
+    public void Loss()
+    {
+        var slot = new ReSlotMachine(new List<List<string>>()
+        {
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"K", "J", "Q", "10", "A","9"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+            new List<string>(){"J", "Q", "K", "9", "10","A"},
+        });
+        var win = slot.Calculate(10);
+        Assert.Equal(0, win);
+    }
+}
