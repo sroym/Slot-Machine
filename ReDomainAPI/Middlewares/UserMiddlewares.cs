b61d09c5c7971c6d97305189153cf1e6202a2705
ZEROSUM\roy <roy0983617927@icloud.com>
2026-06-25 11:07:29 +0800

    有username_把User放進HttpContext green

--- ReDomainAPI/Middlewares/UserMiddlewares.cs
+++ ReDomainAPI/Middlewares/UserMiddlewares.cs
@@ -1,6 +1,26 @@
+using Domain.Gateway;
+
 namespace ReDomainAPI.Middlewares;
 
-public class UserMiddlewares
+public class UserMiddleware
 {
-    
+    private readonly RequestDelegate _next;
+    private readonly UserRepositoryGateway _userRepository;
+
+    public UserMiddleware(RequestDelegate next, UserRepositoryGateway userRepository)
+    {
+        _next = next;
+        _userRepository = userRepository;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var username = context.User.FindFirst(System.Security.Claims.ClaimTypes.Name)?.Value;
+        if (username != null)
+        {
+            var user = _userRepository.FindFromToken(username);
+            context.Items["User"] = user;
+        }
+        await _next(context);
+    }
 }
